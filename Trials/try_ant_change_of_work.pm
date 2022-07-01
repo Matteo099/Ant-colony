82c37cdfa75144a8b27d0327f1188ee5ade2d840
Matteo Romagnoli <matteo02.romagnoli@studenti.unicam.it>
2022-07-01 16:02:32 +0200

    Defined some system cases.

--- Trials/try_ant_change_of_work.pm
+++ Trials/try_ant_change_of_work.pm
@@ -1,16 +1,22 @@
+/*
+  Expected behavior: 
+  - if there is a lot of food, the Foragers are more likely to become Nurses
+  - if there is a little food, the Nurses are more likely to become Foragers 
+*/
+
 const ENERGY = 10;
 const FOOD_STORAGE = 10;
 
 species Q of [0, ENERGY]*[0, FOOD_STORAGE]; 	/* Queen    : Q[i, j] queen with hungry level i, and j is the food storage */
 species N of [0, ENERGY]; 		                /* Nurse    : N[i] hunger of the nurse ant */
 species F of [0, ENERGY]; 		                /* Forager  : F[i] hunger of the forager ant */
-species D;
 
 /* ======================================= */
 /*                  LABELS                 */
 /* ======================================= */
 label nurses = { N[i for i in [0,ENERGY]] }
 label foragers = { F[i for i in [0,ENERGY]] }
+label workers = { N[i for i in [0,ENERGY]], F[i for i in [0,ENERGY]] }
 
 /* Change of work rules */
 rule nurse_becomes_forager for i in [0, ENERGY/2] and j in [0, ENERGY] and f in [0, FOOD_STORAGE] {
@@ -29,16 +35,32 @@ measure n_forager = #foragers;
 measure p_nurse = %nurses;
 measure p_forager = %foragers;
 
+/* Balanced system with a lot of food; Expectation: 5 Foragers, 15 Nurses */
+system balancedFoodH = Q[0, 9]<1>
+                        |N[0]<10>
+                        |F[0]<10>;
 
-system balanced = Q[0, 9]<1>
-                    |N[0]<10>
-                    |F[0]<10>;
+/* Balanced system with a little food; Expectation: 5 Nurses, 15 Foragers */
+system balancedFoodL = Q[0, 1]<1>
+                        |N[0]<10>
+                        |F[0]<10>;
 
-system unbalanced = Q[0, 2]<1>
-                    |N[0]<15>
-                    |F[0]<5>
-                    |D<100>;
+/* Balanced system with half food; Expectation: 10 Nurses, 10 Foragers */
+system balancedFoodM = Q[0, 5]<1>
+                        |N[0]<10>
+                        |F[0]<10>;
 
-system balancedDifferentEnergy = Q[0, 9]<1>
-                    |N[0]<2>|N[2]<2>|N[4]<2>|N[6]<2>|N[8]<2>
-                    |F[0]<2>|F[2]<2>|F[4]<2>|F[6]<2>|F[8]<2>;
+/* Unbalanced system with a lot of food;  Expectation: 5 Foragers, 15 Nurses */
+system unbalancedFoodH = Q[0, 9]<1>
+                        |N[0]<5>
+                        |F[0]<15>;
+
+/* Unbalanced system with a little food; Expectation: 5 Nurses, 15 Foragers */
+system unbalancedFoodL = Q[0, 1]<1>
+                        |N[0]<5>
+                        |F[0]<15>;
+
+/* Unbalanced system with half food; Expectation: 10 Nurses, 10 Foragers */
+system unbalancedFoodM = Q[0, 5]<1>
+                        |N[0]<5>
+                        |F[0]<15>;
